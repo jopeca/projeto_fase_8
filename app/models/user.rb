1ddf396bdf3db3d9e7de18530c502b648f633891
jopeca <carvalho2004@gmail.com>
2019-03-30 13:58:00 +0000

    adicionando token de autenticação

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+    
+    validates_uniqueness_of :auth_token
+    before_create :generate_authentication_token!
+    
+    def info
+        "#{email} - #{create_at} - #{token} -  #{Devise.friendly_token}"
+    end
+    
+    def generate_authentication_token!
+        begin
+            self.auth_token = Devise.friendly_token
+        end while User.exists?(auth_token: auth_token)
+    end
 end
